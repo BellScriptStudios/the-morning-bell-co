4df363dd2e7fc3daa17336a6fa6e2559c44f0ff8
MTinsley00 <dev@bellscript.studio>
2025-08-16 14:47:00 -0400

    feat: build header and footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p>© {new Date().getFullYear()} The Morning Bell Co.</p>
+            <p>All rights reserved</p>
+        </footer>
+    )
+}
